86c73428d738a39fb7b3e991e7c56788fd2baad9
adarsh <adarshgajbhiye86@gmail.com>
2022-12-30 16:21:14 +0530

    Changes in main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
       Sort a = new Sort();
       int[] unsorted =a.setArray();
-      System.out.println("unsorted array is: "+ Arrays.toString(unsorted));
+      System.out.println("unsorted array is 123: "+ Arrays.toString(unsorted));
       int[] sorted = a.sort(unsorted);
       System.out.println("sorted array is: "+Arrays.toString(sorted));
     }
